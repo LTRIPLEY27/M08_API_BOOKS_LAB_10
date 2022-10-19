2c6945eb0a604e61fdbefca492c60bbb8d7fb38d
isaDevs <isabel.calzadilla.18@gmail.com>
2022-10-19 17:31:54 +0200

    finally work with android the loadermanager ---> ok

---
+++ app/src/main/java/isabelcalzadilla/ioc/whowroteit/BookLoader.java
@@ -0,0 +1,35 @@
+package isabelcalzadilla.ioc.whowroteit;
+
+import android.content.Context;
+
+import androidx.loader.content.AsyncTaskLoader;
+
+import org.jetbrains.annotations.Nullable;
+
+// TASK 4 : 4.1 --> Crear clase AsycTaskLoader
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    //4.2.3 VARIABLES DE CLASE
+    private String mQueryString;
+
+    // CONSTRUCTOR
+    public BookLoader(Context context, String query) {
+        super(context);
+        this.mQueryString = query;
+    }
+
+    //4.2.4 LLAMADO AL 'NetworkUtils'
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        // RECIBE LA QUERYSTRING DEFINIDA EN LA ACTIVITYMAIN
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    // TASK 4 : 4.2 --> mÉTODOS NECESARIOS
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+        super.onStartLoading();
+    }
+}
--- app/src/main/java/isabelcalzadilla/ioc/whowroteit/MainActivity.java
+++ app/src/main/java/isabelcalzadilla/ioc/whowroteit/MainActivity.java
@@ -9,9 +9,18 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+// TASK 4.3 : Modificación del MainActivity con LoaderManager
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     // VARIABLES PARA USER INPUT
     private EditText mBookInput;
@@ -28,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         mBookInput = (EditText)findViewById(R.id.bookInput);
         mTitleText = (TextView)findViewById(R.id.titleText);
         mAuthorText = (TextView)findViewById(R.id.authorText);
+
+        if(getSupportLoaderManager().getLoader(0)!=null){
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
+
     }
 
 
@@ -57,8 +71,13 @@ public class MainActivity extends AppCompatActivity {
         // CONDICIONALES PARA VALIDAR : CONEXIÓN, QUERY,
         if (infoRed != null && infoRed.isConnected() && queryString.length() != 0) {
             // LLAMADO AL TASK EN SEGUNDO PLANO
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
-            // TASK 3 : 3.1 -- 'Limpia la búsqueda'
+            //new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+            // TASK 4.3.3 : DECLARACIÓN DEL BUNDLE PARA EL LOADER
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
+
             clean(0);
         }else{
             if(queryString.length() == 0){
@@ -90,5 +109,80 @@ public class MainActivity extends AppCompatActivity {
                 mTitleText.setText(R.string.no_network);
                 break;
         }
+    }
+
+    // TASK 4.3.2 : Métodos de la Inteface 'loadermanager'
+    // 'onCreateLoader' : LLAMADO PARA INSTANCIAR EL LOADER
+    // TASK 4.4.1 : IMPLEMENTACIÓN DE CALLBACKS
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+
+        if(args != null){
+            queryString = args.getString("queryString");
+        }
+        return new BookLoader(this, queryString);
+    }
+
+    // 'onLoadFinished' : LLAMADO AL FINALIZAR LA TAREA EN SEGUNDO PLANO
+    // TASK 4.4.2 : IMPLEMENTACIÓN DE DEL MÉTODO ONPOSTEXECUTE DE FETCHBOOK
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            // Convert the response into a JSON object.
+            JSONObject jsonObject = new JSONObject(data);
+            // Get the JSONArray of book items.
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            // Initialize iterator and results fields.
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            // Look for results in the items array, exiting when both the
+            // title and author are found or when all items have been checked.
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+
+            // If both are found, display the result.
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+                //mBookInput.setText("");
+            } else {
+                // If none are found, update the UI to show failed results.
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+
+        } catch (Exception e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+    // 'onLoaderReset' : LLAMADO para limpiar remanentes
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
